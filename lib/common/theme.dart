38bcf3a009ddd73a6c44e1ee3ba819c3a63e80f6
mohiminulsemon <mohiminulsemon80@gmail.com>
2025-07-03 22:23:38 +0600

    changes in theme data

--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -2,21 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static final ThemeData lightTheme = ThemeData(
-    brightness: Brightness.light,
-    primarySwatch: Colors.teal,
-    scaffoldBackgroundColor: const Color(0xFF0f0f2d),
-    inputDecorationTheme: const InputDecorationTheme(
-      filled: true,
-      fillColor: Color(0xFFF5F5F5),
-      contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 14),
-      border: OutlineInputBorder(),
-      focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Colors.teal, width: 1.5),
-      ),
-    ),
-  );
-
-  static final ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
     primarySwatch: Colors.teal,
     scaffoldBackgroundColor: const Color(0xFF0f0f2d),
@@ -26,9 +11,7 @@ class AppTheme {
       fillColor: Colors.white10,
       contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
       labelStyle: const TextStyle(color: Colors.white70),
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
       enabledBorder: OutlineInputBorder(
         borderSide: const BorderSide(color: Colors.white24),
         borderRadius: BorderRadius.circular(12),
